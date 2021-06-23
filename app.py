134caff4df1318a0ef7b6be43aaa05ca42fd0382
Jorge Martinez <jorgemg0327@gmail.com>
2021-06-23 10:00:33 -0500

    add countries endpoint

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
+import psycopg2
 from fastapi import FastAPI
+from configparser import ConfigParser
+from psycopg2.extras import DictCursor
+from geojson_pydantic import FeatureCollection
 
 app = FastAPI()
+config = ConfigParser()
+config.read("config.txt")
 
-@app.get("/user/{user_id}")
-def get_user(user_id: int):
-	return user_id
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/countries", response_model=FeatureCollection)
+def get_countries():
+    db_params = dict(config.items("PG"))
+    conn = psycopg2.connect(**db_params)
+
+    cur = conn.cursor(cursor_factory=DictCursor)
+    cur.execute(
+        """
+        with t1 as (
+            SELECT
+                name,
+                tags,
+                (ST_DUMP(boundary)).geom AS geom
+            FROM geoboundaries where priority = true),
+        t2 AS (
+            SELECT
+                name,
+                ST_collect(array_agg(geom)) AS geom,
+                tags
+            FROM t1 GROUP BY name, tags)
+        SELECT json_build_object('type',
+            'FeatureCollection',
+            'features',
+            json_agg(ST_ASGEOJSON(t2.*)::json))
+        FROM t2
+        """
+    )
+    result = cur.fetchall()[0][0]
+
+    return FeatureCollection(**result)
