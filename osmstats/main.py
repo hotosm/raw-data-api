39732dac7de17926df6ceb98913f1332b659028d
Jorge Martinez <jorgemg0327@gmail.com>
2021-08-27 17:56:32 -0500

    Authentication with oauth2

--- osmstats/main.py
+++ osmstats/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 
 from .countries.routers import router as countries_router
 from .changesets.routers import router as changesets_router
+from .data.routers import router as data_router
+
 
 app = FastAPI()
 
 app.include_router(countries_router)
 app.include_router(changesets_router)
+app.include_router(data_router)
