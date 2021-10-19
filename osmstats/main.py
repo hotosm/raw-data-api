79189a8dba6ec6a4e1253b7bcc572bac263a3d24
Kshitij Raj Sharma <36752999+itskshitiz321@users.noreply.github.com>
2021-10-19 21:30:00 +0545

    added cors origin

--- osmstats/main.py
+++ osmstats/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .countries.routers import router as countries_router
 from .changesets.routers import router as changesets_router
@@ -8,6 +9,19 @@ from .mapathon.routers import router as mapathon_router
 
 
 app = FastAPI()
+
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(countries_router)
 app.include_router(changesets_router)
