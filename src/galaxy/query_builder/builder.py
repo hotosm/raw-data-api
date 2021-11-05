e0051e74af6bdc8702f4f322bc96a94b043699d2
itskshitiz321 <skshitizraj@gmail.com>
2021-11-05 18:51:09 +0545

    added license

--- src/galaxy/query_builder/builder.py
+++ src/galaxy/query_builder/builder.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2021 Humanitarian OpenStreetmap Team
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Humanitarian OpenStreetmap Team
+# 1100 13th Street NW Suite 800 Washington, D.C. 20005
+# <info@hotosm.org>
+
 from psycopg2 import sql
 
 HSTORE_COLUMN = "tags"
@@ -164,7 +183,6 @@ def data_quality_query(params):
     #             change_id IN (%s)
     # """ % (issue_types, change_ids)
     '''Normal Query to feed our OUTPUT Class '''
-
     query = """
         SELECT osm_id as Osm_id,
                 change_id as Changeset_id,
