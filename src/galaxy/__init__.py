08d274a8222aa20ba22b8ca15a17eb102f496f98
eternaltyro <eternaltyro@protonmail.ch>
2021-12-07 10:00:09 +0000

    Fix syntax error

--- src/galaxy/__init__.py
+++ src/galaxy/__init__.py
@@ -17,10 +17,10 @@ def get_db_connection_params() -> dict:
     """
 
     connection_params = {
-        "POSTGRES_USER": os.getenv("POSTGRES_USER")
-        "POSTGRES_PASSWORD": os.getenv("POSTGRES_PASSWORD")
-        "POSTGRES_HOST": os.getenv("POSTGRES_HOST")
-        "POSTGRES_PORT": os.getenv("POSTGRES_PORT")
+        "POSTGRES_USER": os.getenv("POSTGRES_USER"),
+        "POSTGRES_PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "POSTGRES_HOST": os.getenv("POSTGRES_HOST"),
+        "POSTGRES_PORT": os.getenv("POSTGRES_PORT"),
         "POSTGRES_DATABASE": os.getenv("POSTGRES_DATABASE")
     }
 
