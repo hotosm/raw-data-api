c63e9f83beadb9a3dc368835c705ce8363b52e93
itskshitiz321 <skshitizraj@gmail.com>
2021-12-24 17:50:10 +0545

    added github action for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
