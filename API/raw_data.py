9785924748b67fcfaec83e20b07f290855fd31a4
itskshtiiz321 <skshitizraj@gmail.com>
2022-03-10 14:49:52 +0000

    reduced function waiting time and migrated to link based approach

--- API/raw_data.py
+++ API/raw_data.py
@@ -34,6 +34,9 @@ import os
 from starlette.background import BackgroundTasks
 from .auth import login_required
 from src.galaxy import config
+from os.path import exists
+from area import area
+import json
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
 
@@ -43,18 +46,30 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
 #     result= RawData(params).extract_historical_data()
 #     return generate_rawdata_response(result,start_time)
 
+@router.get("/exports/{file_name}")
+async def download_export(file_name: str,background_tasks: BackgroundTasks):
+    """Used for Delivering our export to user , It will hold the zip file until user downloads or hits the url once it is delivered it gets cleared up, Designed as  a separate function to avoid the condition ( waiting for the api response without knowing what is happening on the background )
+    Returns zip file if it is present on our server if not returns null 
+    """
+    zip_temp_path=f"""exports/{file_name}.zip"""
+    if exists(zip_temp_path):
+        response = FileResponse(zip_temp_path,media_type="application/zip")
+        response.headers["Content-Disposition"] = f"attachment; filename={file_name}.zip"
+        background_tasks.add_task(remove_file, zip_temp_path) #clearing the tmp zip file
+        return response
+
 def remove_file(path: str) -> None:
     """Used for removing temp file after zip file is delivered to user
     """
     os.unlink(path)
 
 @router.post("/current-snapshot/")
-def get_current_data(params:RawDataCurrentParams,background_tasks: BackgroundTasks):  
+async def get_current_data(params:RawDataCurrentParams,background_tasks: BackgroundTasks):  #using async function so that multiple request at a time won't get stuck
 # def get_current_data(params:RawDataCurrentParams,background_tasks: BackgroundTasks, user_data=Depends(login_required)):
     start_time = time.time()
     logging.debug('Request Received from Raw Data API ')
     exportname =f"Raw_Export_{datetime.now().isoformat()}"
-    dump_geojson_temp_file= RawData(params).extract_current_data(exportname)
+    dump_geojson_temp_file=RawData(params).extract_current_data(exportname)
     logging.debug('Zip Binding Started !')
     # in_memory = BytesIO()
     
@@ -67,16 +82,13 @@ def get_current_data(params:RawDataCurrentParams,background_tasks: BackgroundTas
     
     zf.close()
     logging.debug('Zip Binding Done !')
-    # response = StreamingResponse(zip_temp_path,media_type="application/zip")
-    # # response.headers["Content-Disposition"] = f"attachment; filename={exportname}.zip"
-    print("-----Raw Data Request Took-- %s seconds -----" % (time.time() - start_time))
-    # background_tasks.add_task(remove_file, zip_temp_path) #clearing the tmp zip file 
-    background_tasks.add_task(remove_file, dump_geojson_temp_file) # clearing tmp geojson file 
-    
+    logging.debug("-----Raw Data Request Took-- %s seconds -----" % (time.time() - start_time))
+    background_tasks.add_task(remove_file, dump_geojson_temp_file) # # clearing tmp geojson file since it is already dumped to zip file we don't need it anymore  
+
     client_host = dict(config.items("HOST"))['host'] #getting hosting url 
     client_port = dict(config.items("HOST"))['port'] #getting hosting port
 
-    download_url=f"""{client_host}:{client_port}/{zip_temp_path}"""
+    download_url=f"""{client_host}:{client_port}/raw-data/exports/{exportname}""" # disconnected download portion from this endpoint because when there will be multiple hits at a same time we don't want function to get stuck waiting for user to download the file and deliver the response , we want to reduce waiting time and free function ! 
     response_time=time.time() - start_time
-    return {"download_url": download_url, "response_time": f"""{int(response_time)} Seconds"""}
+    return {"download_url": download_url, "response_time": f"""{int(response_time)} Seconds""", "query_area" : f"""{int(area(json.loads(params.geometry.json()))* 1E-6)} Sq Km"""}
     
