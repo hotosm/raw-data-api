864fa2b10894d476490853ecb27bf90db4692013
itskshtiiz321 <skshitizraj@gmail.com>
2022-03-24 08:17:39 +0000

    changed mbtiles to 2sqkm

--- src/galaxy/validation/models.py
+++ src/galaxy/validation/models.py
@@ -503,7 +503,7 @@ class RawDataCurrentParams(BaseModel):
         output_type = values.get("output_type")
         if output_type:
             if output_type is RawDataOutputType.MBTILES.value: # for mbtiles ogr2ogr does very worst job when area gets bigger we should write owr own or find better approach for larger area
-                RAWDATA_CURRENT_POLYGON_AREA=5 # we need to figure out how much tile we are generating before passing request on the basis of bounding box we can restrict user , right now relation contains whole country for now restricted to this area but can not query relation will take ages because that will intersect with country boundary : need to clip it 
+                RAWDATA_CURRENT_POLYGON_AREA=2 # we need to figure out how much tile we are generating before passing request on the basis of bounding box we can restrict user , right now relation contains whole country for now restricted to this area but can not query relation will take ages because that will intersect with country boundary : need to clip it 
         if area_km2 > RAWDATA_CURRENT_POLYGON_AREA:
                 raise ValueError(f"""Polygon Area {int(area_km2)} Sq.KM is higher than {RAWDATA_CURRENT_POLYGON_AREA} Sq.KM""")
         return value
