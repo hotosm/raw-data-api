9302bcbd1681a5347455de427d90a43339a4916a
itskshitiz321 <skshitizraj@gmail.com>
2022-05-12 11:04:41 +0545

    added validation and request params #209

--- src/galaxy/validation/models.py
+++ src/galaxy/validation/models.py
@@ -489,9 +489,79 @@ class OsmElementRawData(Enum):
     WAYS = "ways"
     RELATIONS = "relations"
 
+class SupportedFilters(Enum):
+    TAGS = "tags"
+    ATTRIBUTES = "attributes" 
+    
+    @classmethod
+    def has_value(cls, value):
+        return value in cls._value2member_map_ 
 
+class SupportedGeometryFilters(Enum):
+    POINT='point'
+    LINE='line'
+    POLYGON='polygon'
+    ALLGEOM='all_geometry'
+
+    @classmethod
+    def has_value(cls, value):
+        return value in cls._value2member_map_ 
 
 class RawDataCurrentParams(BaseModel):
+    output_type : Optional[RawDataOutputType]=None
+    file_name : Optional[str]=None
+    geometry : Polygon
+    filters : Optional[dict]=None
+    geometry_type : Optional[List[GeometryTypeRawData]] = None
+    
+    @validator("filters", allow_reuse=True)
+    def check_value(cls, value, values):
+        for key, v in value.items():
+            if SupportedFilters.has_value(key): # check for tags or attributes
+                if isinstance(v, dict):  # check if value is of dict type or not for tags and attributes 
+                    for k, val in v.items():
+                        if SupportedGeometryFilters.has_value(k):# now checking either point line or polygon
+                            if key == SupportedFilters.TAGS.value: # if it is tag then value should be of dictionary
+                                if isinstance(val, dict):
+                                    # if it is dictionary it should be of type key:['value']
+                                    for osmkey,osmvalue in val.items():
+                                        if isinstance(osmvalue, list):
+                                            pass
+                                        else:
+                                            raise ValueError(f"""Osm value --{osmvalue}-- should be inside List : {key}-{k}-{val}-{osmvalue}""")
+                                else:
+                                    raise ValueError(f"""Type of {val} filter in {key} - {k} - {val} should be dictionary""")
+                            elif key ==  SupportedFilters.ATTRIBUTES.value:
+                                if isinstance(val, list): # if it is attributes value should be of list
+                                    pass
+                                else:
+                                    raise ValueError(f"""Type of {val} filter in {key} - {k} - {val} should be list""")
+                        else :
+                            raise ValueError(f"""Value {k} for filter {key} - {k} is not supported""")
+                else :
+                    raise ValueError(f"""Value for filter {key} should be of dict Type""")
+            else :
+                raise ValueError(f"""Filter {key} is not supported. Supported filters are 'tags' and 'attributes'""")
+        return value
+
+    @validator("geometry", always=True)
+    def check_geometry_area(cls, value, values):
+        area_m2 = area(json.loads(value.json()))
+        area_km2 = area_m2 * 1E-6
+        try :
+            RAWDATA_CURRENT_POLYGON_AREA=int(config.get("EXPORT_CONFIG", "max_area"))
+        except: 
+            RAWDATA_CURRENT_POLYGON_AREA=100000 
+        output_type = values.get("output_type")
+        if output_type:
+            if output_type is RawDataOutputType.MBTILES.value: # for mbtiles ogr2ogr does very worst job when area gets bigger we should write owr own or find better approach for larger area
+                RAWDATA_CURRENT_POLYGON_AREA=2 # we need to figure out how much tile we are generating before passing request on the basis of bounding box we can restrict user , right now relation contains whole country for now restricted to this area but can not query relation will take ages because that will intersect with country boundary : need to clip it 
+        if area_km2 > RAWDATA_CURRENT_POLYGON_AREA:
+                raise ValueError(f"""Polygon Area {int(area_km2)} Sq.KM is higher than Threshold : {RAWDATA_CURRENT_POLYGON_AREA} Sq.KM""")
+        return value
+
+
+class RawDataCurrentoldParams(BaseModel):
     output_type : Optional[RawDataOutputType]=None
     file_name : Optional[str]=None
     geometry : Polygon
