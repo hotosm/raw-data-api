d02b769b26baf57e4379c43ed5ed55f91b96a630
Kshitij Raj Sharma <skshitizraj@gmail.com>
2022-10-13 18:43:41 +0545

    changed yml

--- populate-docker-db.sh
+++ populate-docker-db.sh
@@ -1,6 +1,3 @@
-  #!/bin/bash
-  psql -U postgres  insights < /sql/insights.sql
-  psql -U postgres  insights < /sql/mapathon_summary.sql
-  psql -U postgres  raw  < /sql/raw_data.sql
-  psql -U postgres  underpass < /sql/underpass.sql
-  psql -U postgres  tm < /sql/tasking-manager.sql
+#!/bin/bash
+psql -U postgres  raw  < /sql/raw_data.sql
+psql -U postgres  underpass < /sql/underpass.sql
