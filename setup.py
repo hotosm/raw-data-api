b45ec019e42dc02bf34d4a0fa25628a180908af2
kshitijrajsharma <skshitizraj@gmail.com>
2022-12-07 15:18:54 +0545

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="export-tool-api",
-    version="0.0.1",
+    version="1.0.12",
     description="The export tool api module makes it simple for you to get osm data stats provided by api in your own project",
     packages=["src"],
     package_dir={"export_tool_api": "src"},
