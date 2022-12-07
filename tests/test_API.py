3e9061a20f3b7dca3e235618ee94ce7ef6565680
kshitijrajsharma <kshitijrajsharma@users.noreply.github.com>
2022-12-07 06:30:49 +0000

    :art: Format Python code with psf/black

--- tests/test_API.py
+++ tests/test_API.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-import time 
+import time
 from API.main import app
 
 client = TestClient(app)
@@ -9,24 +9,28 @@ def test_status():
     response = client.get("/latest/status/")
     assert response.status_code == 200
 
+
 def test_snapshot():
-    response = client.post("/latest/snapshot/", json={
-                    "geometry": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [
-                                [83.96919250488281, 28.194446860487773],
-                                [83.99751663208006, 28.194446860487773],
-                                [83.99751663208006, 28.214869548073377],
-                                [83.96919250488281, 28.214869548073377],
-                                [83.96919250488281, 28.194446860487773],
-                            ]
-                        ],
-                    }
-                })
+    response = client.post(
+        "/latest/snapshot/",
+        json={
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [83.96919250488281, 28.194446860487773],
+                        [83.99751663208006, 28.194446860487773],
+                        [83.99751663208006, 28.214869548073377],
+                        [83.96919250488281, 28.214869548073377],
+                        [83.96919250488281, 28.194446860487773],
+                    ]
+                ],
+            }
+        },
+    )
     assert response.status_code == 200
     res = response.json()
-    track_link = res['track_link']
+    track_link = res["track_link"]
     time.sleep(6)
     response = client.get(f"/latest{track_link}")
     assert response.status_code == 200
@@ -34,4 +38,3 @@ def test_snapshot():
     check_status = res["status"]
     print(check_status)
     assert check_status == "SUCCESS"
-
