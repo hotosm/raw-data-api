4a905b9163db97995ad932d45870cd0789ca3f87
eternaltyro <eternaltyro@protonmail.ch>
2023-02-07 11:04:30 +0000

    Fix issues flagged by TF

--- infra/variables.tf
+++ infra/variables.tf
@@ -51,7 +51,7 @@ variable "disk_size" {
 variable "server_skus" {
   type = map(any)
   default = {
-    database = "D2ds_v4"
+    database = "Standard_D2ds_v4"
     backend  = "Standard_F2"
   }
 }
