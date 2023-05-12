171b786ea2b22dca53030c155570b0cb42bcf032
kshitijrajsharma <skshitizraj@gmail.com>
2023-05-12 12:01:17 +0545

    Get rid of concurrently as it reduced the performance

--- backend/sql/post_indexes.sql
+++ backend/sql/post_indexes.sql
@@ -24,19 +24,19 @@ create extension if not exists intarray;
 -- Only apply after grid update is done 
 -- Multi column index is used for the larger datasets which will have large gist index size (such as loading asia, america) to reduce amount of the index to look for spatial query , db with smaller datasets such as only with few grid level data it makes no sense , by default with lua script it will create gist index ordered with geohash method 
 
-CREATE INDEX CONCURRENTLY  IF NOT EXISTS   nodes_country_idx ON public.nodes USING btree (country);
-CREATE INDEX CONCURRENTLY  IF NOT EXISTS  nodes_geom_idx ON public.nodes USING gist (geom);
+CREATE INDEX IF NOT EXISTS   nodes_country_idx ON public.nodes USING btree (country);
+CREATE INDEX IF NOT EXISTS  nodes_geom_idx ON public.nodes USING gist (geom);
 
 
-CREATE INDEX CONCURRENTLY  IF NOT EXISTS  ways_line_country_idx ON public.ways_line USING gin (country gin__int_ops);
-CREATE INDEX CONCURRENTLY  IF NOT EXISTS  ways_line_geom_idx ON public.ways_line USING gist (geom);
+CREATE INDEX IF NOT EXISTS  ways_line_country_idx ON public.ways_line USING gin (country gin__int_ops);
+CREATE INDEX IF NOT EXISTS  ways_line_geom_idx ON public.ways_line USING gist (geom);
 
 
-CREATE INDEX CONCURRENTLY  IF NOT EXISTS  ways_poly_country_idx ON public.ways_poly USING btree (country);
-CREATE INDEX CONCURRENTLY  IF NOT EXISTS  ways_poly_geom_idx ON public.ways_poly USING gist (geom);
+CREATE INDEX IF NOT EXISTS  ways_poly_country_idx ON public.ways_poly USING btree (country);
+CREATE INDEX IF NOT EXISTS  ways_poly_geom_idx ON public.ways_poly USING gist (geom);
 
-CREATE INDEX CONCURRENTLY  IF NOT EXISTS  relations_geom_idx ON public.relations USING gist (geom);
-CREATE INDEX CONCURRENTLY  IF NOT EXISTS  relations_country_idx ON public.relations USING gin (country gin__int_ops);
+CREATE INDEX IF NOT EXISTS  relations_geom_idx ON public.relations USING gist (geom);
+CREATE INDEX IF NOT EXISTS  relations_country_idx ON public.relations USING gin (country gin__int_ops);
 
 -- Clustering command , it is recommended for larger datasets , It will be easy for indexes to find right files faster , will also reduce coutry geom index size since all related rows will be clustered as block 
 
--- backend/sql/pre_indexes.sql
+++ backend/sql/pre_indexes.sql
@@ -18,17 +18,17 @@
 -- # 1100 13th Street NW Suite 800 Washington, D.C. 20005
 -- # <info@hotosm.org>
 
--- CREATE INDEX CONCURRENTLY  IF NOT EXISTS   nodes_uid_idx ON public.nodes USING btree (uid);
--- CREATE INDEX CONCURRENTLY  IF NOT EXISTS   nodes_changeset_idx ON public.nodes USING btree (changeset);
+-- CREATE INDEX IF NOT EXISTS   nodes_uid_idx ON public.nodes USING btree (uid);
+-- CREATE INDEX IF NOT EXISTS   nodes_changeset_idx ON public.nodes USING btree (changeset);
 
--- CREATE INDEX CONCURRENTLY  IF NOT EXISTS  ways_line_uid_idx ON public.ways_line USING btree (uid);
--- CREATE INDEX CONCURRENTLY  IF NOT EXISTS  ways_line_changeset_idx ON public.ways_line USING btree (changeset);
+-- CREATE INDEX IF NOT EXISTS  ways_line_uid_idx ON public.ways_line USING btree (uid);
+-- CREATE INDEX IF NOT EXISTS  ways_line_changeset_idx ON public.ways_line USING btree (changeset);
 
--- CREATE INDEX CONCURRENTLY  IF NOT EXISTS  ways_poly_uid_idx ON public.ways_poly USING btree (uid);
--- CREATE INDEX CONCURRENTLY  IF NOT EXISTS  ways_poly_changeset_idx ON public.ways_poly USING btree (changeset);
+-- CREATE INDEX IF NOT EXISTS  ways_poly_uid_idx ON public.ways_poly USING btree (uid);
+-- CREATE INDEX IF NOT EXISTS  ways_poly_changeset_idx ON public.ways_poly USING btree (changeset);
 
--- CREATE INDEX CONCURRENTLY  IF NOT EXISTS  relations_uid_idx ON public.relations USING btree (uid);
--- CREATE INDEX CONCURRENTLY  IF NOT EXISTS  relations_changeset_idx ON public.relations USING btree (changeset);
+-- CREATE INDEX IF NOT EXISTS  relations_uid_idx ON public.relations USING btree (uid);
+-- CREATE INDEX IF NOT EXISTS  relations_changeset_idx ON public.relations USING btree (changeset);
 
 
 CREATE EXTENSION IF NOT EXISTS btree_gist;
@@ -47,15 +47,15 @@ ALTER TABLE relations
 ADD CONSTRAINT relations_pk PRIMARY KEY (osm_id);
 
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS nodes_timestamp_idx ON public.nodes USING btree ("timestamp");
+CREATE INDEXIF NOT EXISTS nodes_timestamp_idx ON public.nodes USING btree ("timestamp");
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ways_line_timestamp_idx ON public.ways_line USING btree ("timestamp");
+CREATE INDEXIF NOT EXISTS ways_line_timestamp_idx ON public.ways_line USING btree ("timestamp");
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ways_poly_timestamp_idx ON public.ways_poly USING btree ("timestamp");
+CREATE INDEXIF NOT EXISTS ways_poly_timestamp_idx ON public.ways_poly USING btree ("timestamp");
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS relations_tags_idx ON public.relations USING gin (tags);
+CREATE INDEXIF NOT EXISTS relations_tags_idx ON public.relations USING gin (tags);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS relations_timestamp_idx ON public.relations USING btree ("timestamp");
+CREATE INDEXIF NOT EXISTS relations_timestamp_idx ON public.relations USING btree ("timestamp");
 
 
 
