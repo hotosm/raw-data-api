2f2e79b1eb67f742976c910045cc5c84a2c7ed7c
kshitijrajsharma <skshitizraj@gmail.com>
2023-12-22 19:48:30 +0545

    Strip time for finished object

--- API/tasks.py
+++ API/tasks.py
@@ -37,7 +37,9 @@ def get_task_status(task_id):
         "id": task_id,
         "status": task_result.state,
         "result": task_result.result if task_result.status == "SUCCESS" else None,
-        "finished_at": task_result.date_done if task_result.date_done else None,
+        "finished_at": task_result.date_done.strftime("%Y-%m-%dT%H:%M:%S")
+        if task_result.date_done
+        else None,
     }
     return JSONResponse(result)
 
