6b427b9033e127f733cdaee4c5ff817f98aeff8a
KafayatYusuf <155323901+KafayatYusuf@users.noreply.github.com>
2024-03-18 15:49:06 +0100

    404 response added

--- API/tasks.py
+++ API/tasks.py
@@ -80,7 +80,7 @@ def get_task_status(
 
 
 #task revoke
-@router.get("/revoke/{task_id}/")
+@router.get("/revoke/{task_id}/",responses={**common_responses})
 @version(1)
 def revoke_task(task_id, user: AuthUser = Depends(staff_required)):
     """Revokes task , Terminates if it is executing
