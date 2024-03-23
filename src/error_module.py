0a82ea4f2fd0d1b28f04507654b83e87ee6ebdde
KafayatYusuf <155323901+KafayatYusuf@users.noreply.github.com>
2024-03-23 17:18:03 +0100

    Created a directory called "error_module" in src folder to import ErrorMessage, common_responses and login_responses

---
+++ src/error_module.py
@@ -0,0 +1,22 @@
+# error_module.py
+
+class ErrorMessage:
+    def __init__(self, message):
+        self.message = message
+
+    def display(self):
+        print("Error:", self.message)
+
+class common_responses:
+    def __init__(self, message):
+        self.message = message
+
+    def display(self):
+        print("Error:", self.message)
+
+class login_responses:
+    def __init__(self, message):
+        self.message = message
+
+    def display(self):
+        print("Error:", self.message)
