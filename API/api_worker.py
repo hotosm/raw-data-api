909f5410e0b3a50c05daf320b5b32a08872374ab
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-01-29 16:02:56 -0300

    - Remove debug parameter

--- API/api_worker.py
+++ API/api_worker.py
@@ -54,7 +54,7 @@ celery.conf.result_backend = celery_backend
 # celery.conf.result_serializer = "json"
 # celery.conf.accept_content = ["application/json", "application/x-python-serialize"]
 celery.conf.task_track_started = True
-celery.conf.update(result_extended=True, worker_pool_restarts=True)
+celery.conf.update(result_extended=True)
 # celery.conf.task_reject_on_worker_lost = True
 # celery.conf.task_acks_late = True
 
