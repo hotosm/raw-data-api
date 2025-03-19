c70ecd26c162ba7473c28fea35d1fad1c1327ee2
kshitijrajsharma <kshitijrajsharma@users.noreply.github.com>
2025-03-19 09:48:11 +0000

    :art: Format Python code with psf/black

--- src/utils.py
+++ src/utils.py
@@ -17,26 +17,34 @@ def cleanup_old_exports(base_export_dir, days):
     cutoff_time = now - (days * 86400)  # days to seconds
 
     for dirpath, dirnames, filenames in os.walk(base_export_dir):
-
-       # Remove old files
+        # Remove old files
         for filename in filenames:
             file_full_path = os.path.join(dirpath, filename)
-            if os.path.isfile(file_full_path) and os.path.getmtime(file_full_path) < cutoff_time:
+            if (
+                os.path.isfile(file_full_path)
+                and os.path.getmtime(file_full_path) < cutoff_time
+            ):
                 os.remove(file_full_path)
                 logging.info(f"Removed old export file: {file_full_path}")
 
         # Remove old directories
         for dirname in dirnames:
             dir_full_path = os.path.join(dirpath, dirname)
-            if os.path.isdir(dir_full_path) and os.path.getmtime(dir_full_path) < cutoff_time:
+            if (
+                os.path.isdir(dir_full_path)
+                and os.path.getmtime(dir_full_path) < cutoff_time
+            ):
                 shutil.rmtree(dir_full_path)
                 logging.info(f"Removed old export directory: {dir_full_path}")
+
 
 def create_working_dir(working_dir):
     """
     Creates a working directory for exports. If cleanup is enabled, it first removes old directories.
     """
-    base_export_working_dir = os.path.dirname(os.path.dirname(os.path.abspath(working_dir)))  # Get the grandparent directory , because exportdir/uid/export
+    base_export_working_dir = os.path.dirname(
+        os.path.dirname(os.path.abspath(working_dir))
+    )  # Get the grandparent directory , because exportdir/uid/export
     # Check if cleanup is enabled
     cleanup_enabled = ENABLE_OLD_EXPORTS_CLEANUP
     cleanup_days = OLD_EXPORTS_CLEANUP_DAYS
@@ -49,7 +57,9 @@ def create_working_dir(working_dir):
             os.makedirs(working_dir)
         except OSError as e:
             if e.errno == errno.ENOSPC:  # No space left on device
-                logging.warning("No space left on device, attempting to cleanup old exports and retry...")
+                logging.warning(
+                    "No space left on device, attempting to cleanup old exports and retry..."
+                )
                 cleanup_old_exports(base_export_working_dir, cleanup_days)
                 os.makedirs(working_dir)
             else:
