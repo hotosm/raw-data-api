b0d71a32d21ea3a6053536c490231110e3cf6c27
kshitijrajsharma <skshitizraj@gmail.com>
2025-06-14 22:57:49 +0200

    feat(country_h3): add country_h3_flat table and populate with H3 indexes from countries

--- backend/sql/post_indexes.sql
+++ backend/sql/post_indexes.sql
@@ -82,3 +82,21 @@ ANALYZE ways_poly;
 ANALYZE relations;
 
 
+
+CREATE TABLE country_h3_flat (
+    h3_index h3index NOT NULL,
+    country_id INT NOT NULL REFERENCES countries(cid),
+    PRIMARY KEY (h3_index, country_id)
+);
+
+
+WITH h3_indexes AS (
+  SELECT
+    c.cid,
+    h3_index
+  FROM countries c, LATERAL h3_polygon_to_cells(c.geometry, 6) AS h3_index
+)
+INSERT INTO country_h3_flat (h3_index, country_id)
+SELECT h3_index, cid FROM h3_indexes;
+
+
